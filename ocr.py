edd1c7236c798150bdc1be90616910f6d12a0717
James Bush <bushjam@oregonstate.edu>
2023-05-01 15:53:39 -0700

    config ocr

--- ocr.py
+++ ocr.py
@@ -26,7 +26,7 @@ def ocr_img(response):
   """
 
   img = Image.open(BytesIO(response.content))
-  config = r'--oem 3 --psm 6 tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyz' #oem: default OCR engine; psm: assume single block text
+  config = "-c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyz -psm 6" #oem: default OCR engine; psm: assume single block text
   # config = r'--oem 3 --psm 6' #oem: default OCR engine; psm: assume single block text
   text = pytesseract.image_to_string(img, config=config, lang="eng")
   return text
