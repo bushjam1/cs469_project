7e0a187cc8bcca39cc7076e3a1af04a5432cbb06
jfb <bushjam@oregonstate.edu>
2023-05-30 19:00:05 -0700

    more work on tests

--- tests.py
+++ tests.py
@@ -1,37 +1,53 @@
 import requests
+import json 
 
-def runtest(test_num, url):
+def runtest(test_num, img_url, val):
     
     base_url = 'http://10.0.0.1:5000/api?url='
     url = base_url + img_url 
-
     # call the API with image URL, get response or error
-    print("\n","="*80)
+    print("\n")
+    print("="*80)
     print(f"Test number: {test_num}")
     response = requests.get(url)
+    text =  json.loads(response.text)['response']
+    print("URL",url)
     print("Status:",response.status_code)
-    print("Result:",response.json['response'])
-    if response.status_code == 200:
+    #print("Result:", text)
+    if response.status_code == 200 and val in text.lower():
+        print(f"FOUND {val}")
         print("TEST PASSED")
+
+        return True
     else:
-        print("TEST FAILED", response.status_code, response.json)
-
-
+        print("TEST FAILED")
+        print(f"Did not find: {val}")
+        return False
+    
 def tests():
 
-    img_urls = [    'https://live.staticflickr.com/3760/13307397783_5266496226_b.jpg',
-                    'https://live.staticflickr.com/2397/2476584907_73def16be0.jpg',
-                    'https://live.staticflickr.com/5342/13965039173_9fdb82e940_b.jpg'
-                ]
+    img_urls = {
+            'alexanderplatz':'https://live.staticflickr.com/3912/15349926286_992542ec2e.jpg',
+            'account':'https://i.pinimg.com/736x/7b/08/c9/7b08c9eff1bce30f6e13afba65002f10.jpg',                   
+            'turmstrasse':'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/U-Bahn_Turmstra%C3%9Fe%2C_Berlin.jpg/320px-U-Bahn_Turmstra%C3%9Fe%2C_Berlin.jpg',
+            'stop':'http://upload.wikimedia.org/wikipedia/commons/f/f9/STOP_sign.jpg'
+            
+            }
+
+    results = {'pass':0,'fail':0}
     # test 1
-    for i in range(len(img_urls)):
-        runtest(i, img_url[i])
-
-    # test 2 
-    # test 3 
-    # test 4
-    # test 5
-
+    test_seq = 0 
+    for key, val in img_urls.items():
+        test_seq += 1
+        res = runtest(test_seq, val, key)
+        if res is True:
+            results['pass'] += 1
+        else:
+            results['fail'] += 1
+    print()
+    print("="*80)
+    print(f"RESULTS: tests passed: {results['pass']} tests failed: {results['fail']}")
+    print()
 
 if __name__ == "__main__":
     tests()
